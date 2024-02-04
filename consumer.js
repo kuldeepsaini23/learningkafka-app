bbd98024240650977b9b4e37047e29eca5e78a60
kuldeepsaini02311@gmail.com <kuldeepsaini02311@gmail.com>
2024-02-04 18:41:59 +0530

    Refactor consumer and producer to use dynamic group and user input

--- consumer.js
+++ consumer.js
@@ -1,19 +1,20 @@
 const { kafka } = require("./client");
+const group = process.argv[2];
 
 async function init() {
-  const consumer = kafka.consumer({ groupId: "user1" });
+  const consumer = kafka.consumer({ groupId: group });
   // Connecting Consumer
   console.log("Consumer Connecting...");
   await consumer.connect();
   console.log("Consumer Connected Successfully!");
 
   // Subscribing to Topic
-  await consumer.subscribe({ topics: ["riders-updates"], fromBeginning: true});
+  await consumer.subscribe({ topics: ["riders-updates"], fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
       console.log(
-        `[${topic}]: Partition: ${partition} | Offset: ${
+        `${group}: [${topic}]: Partition: ${partition} | Offset: ${
           message.offset
         } | Value: ${message.value.toString()} `
       );
--- producer.js
+++ producer.js
@@ -1,5 +1,10 @@
 const { kafka } = require("./client");
+const readline = require("readline");
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 async function init() {
   const producer = kafka.producer();
   console.log("Producer Connecting...");
@@ -12,44 +17,71 @@ async function init() {
       console.log("Error in Connecting Producer: ", err);
     });
 
+  // Takin input from user
+  rl.setPrompt("Enter your message>: ");
+  rl.prompt();
+
+  rl.on("line", async function (message) {
+    const [ridername, location] = message.split(" ");
+    await producer
+      .send({
+        topic: "riders-updates",
+        messages: [
+          {
+            partition: location.toLowerCase() === "north" ? 0 : 1,
+            key: "location-updates",
+            value: JSON.stringify({ naem: ridername, loc: location }),
+          },
+        ],
+      })
+      .then((data) => {
+        console.log("Message Send successfully: ", data);
+      })
+      .catch((err) => {
+        console.log("Error in sending message: ", err);
+      });
+  }).on('close', async()=>{
+    await producer.disconnect();
+  })
+
   // Sending Messages
-  await producer
-    .send({
-      topic: "riders-updates",
-      messages: [
-        {
-          partition: 0,
-          key: "location-updates",
-          value: JSON.stringify({ naem: "Tony Stark", loc: "SOUTH" }),
-        },
-        {
-          partition: 0,
-          key: "location-updates",
-          value: JSON.stringify({ naem: "Kuldeep Saini", loc: "Mumbai" }),
-        },
-        {
-          partition: 0,
-          key: "location-updates",
-          value: JSON.stringify({ naem: "Captain America", loc: "Delhi" }),
-        },
-      ],
-    })
-    .then((data) => {
-      console.log("Message Send successfully: ", data);
-    })
-    .catch((err) => {
-      console.log("Error in sending message: ", err);
-    });
+  // await producer
+  //   .send({
+  //     topic: "riders-updates",
+  //     messages: [
+  //       {
+  //         partition: 0,
+  //         key: "location-updates",
+  //         value: JSON.stringify({ naem: "Tony Stark", loc: "SOUTH" }),
+  //       },
+  //       {
+  //         partition: 0,
+  //         key: "location-updates",
+  //         value: JSON.stringify({ naem: "Kuldeep Saini", loc: "Mumbai" }),
+  //       },
+  //       {
+  //         partition: 0,
+  //         key: "location-updates",
+  //         value: JSON.stringify({ naem: "Captain America", loc: "Delhi" }),
+  //       },
+  //     ],
+  //   })
+  //   .then((data) => {
+  //     console.log("Message Send successfully: ", data);
+  //   })
+  //   .catch((err) => {
+  //     console.log("Error in sending message: ", err);
+  //   });
 
   // Disconnecting Producer
-  await producer
-    .disconnect()
-    .then(() => {
-      console.log("Producer Disconnected Successfully!");
-    })
-    .catch((err) => {
-      console.log("Error in dissconnecting Producer: ", err);
-    });
+  // await producer
+  //   .disconnect()
+  //   .then(() => {
+  //     console.log("Producer Disconnected Successfully!");
+  //   })
+  //   .catch((err) => {
+  //     console.log("Error in dissconnecting Producer: ", err);
+  //   });
 }
 
-init(); 
+init();
